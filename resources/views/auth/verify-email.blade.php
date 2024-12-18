7211faf2d266be380d8ac7ae5f7dc639f4ea026c
DummyCoding <modestopellerin@gmail.com>
2024-12-19 01:58:42 +0800

    Center title

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -4,9 +4,10 @@
             <x-flashMsg msg="{{ session('success') }}"/>
     @endif
 
-    <h1 class="title">Verify your email</h1>
+    <h1 class="mx-auto max-w-screen-sm title">Verify your email</h1>
 
-    <h1 class="mb-4">Please verify your email through the email PonPon sent you.</h1>
+    <div class="mx-auto max-w-screen-sm card">
+        <h1 class="mb-4">Please verify your email through the email PonPon sent you.</h1>
 
     <p class="mb-2">Didn't get the email?</p>
     <form action="{{ route('verification.send') }}" method="post">
@@ -14,5 +15,8 @@
 
         <button class="btn">Send again</button>
     </form>
+    </div>
+
+
 
 </x-layout>
